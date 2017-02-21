6e4998f3867872d0930f0f55537ce5e5473d35d1
Daniel <d.nitulescu@yahoo.com>
2017-02-21 20:19:42 +0200

    Version0

--- PythonTestCode.py
+++ PythonTestCode.py
@@ -1,13 +1,16 @@
 import cv2
 
-img = cv2.imread("galaxy.jpg",0)
-print(type(img))
-print(img)
-print(img.shape)
-print(img.ndim)
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-img_resized = cv2.resize(img,(int(img.shape[1]/2),int(img.shape[0]/2)))
-cv2.imshow("Galaxy",img_resized)
-cv2.imwrite("Galaxy_resized.jpg",img_resized)
+img = cv2.imread("Pictures/photo.jpg")
+
+gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+faces = face_cascade.detectMultiScale(gray_img,scaleFactor=1.05,minNeighbors=5)
+
+for x,y,w,h in faces:
+    img = cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
+
+
+cv2.imshow("photo",img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
